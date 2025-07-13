9d39e458219e9e85a499dc2ed87eca4391694915
YASHWANT CHAWHAN <ynchawhan4@gmail.com>
2025-07-13 20:17:25 +0400

    Setting up modular structure

---
+++ packages/features/venue_list/lib/venue_list.dart
@@ -0,0 +1,3 @@
+library venue_list;
+
+export 'package:venue_list/venue_list_screen.dart';
---
+++ packages/features/venue_list/lib/venue_list_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class VenueListScreen extends StatelessWidget {
+  const VenueListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+       MaterialApp(
+        home: Container(child: Text("Privilee"),),
+      );
+  }
+}
