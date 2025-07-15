56576b9a076cb063a06f6a455c6fdee439796333
YASHWANT CHAWHAN <ynchawhan4@gmail.com>
2025-07-15 07:17:00 +0400

    remove map key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAHUzgZxa7nbUbYoyIns-8-KIcm7G43RDU")
+    GMSServices.provideAPIKey("ADD YOUR MAP KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
